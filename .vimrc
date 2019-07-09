70b181835ed936730d15c61df89803a5c85e3274
QAQmander <salamander@buaa.edu.cn>
2019-07-09 22:40:44 +0800

    add .vimrc

---
+++ .vimrc
@@ -0,0 +1,13 @@
+set nocompatible
+
+map <O> ko
+imap <C-b> <ESC>i
+imap <C-f> <ESC>la
+
+syntax on
+
+set cindent
+set shiftwidth=4
+set expandtab
+
+set noswapfile
--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ git clone https://github.com/qaqmander/qpwn.git /tmp/qpwn
  
 printf '%s' "fix ~/.bashrc: add /pwn/bin to PATH..."
 # TODO: maybe there is some way to change $PATH immediately
-echo '\nexport PATH=$PATH:/qpwn/bin' >> $HOME/.bashrc #&& source $HOME/.bashrc
+echo '\nexport PATH=$PATH:/qpwn/bin' >> ~/.bashrc #&& source $HOME/.bashrc
 printf '  %s\n' 'done' 
 echo '[NOTICE] You need to source ~/.bashrc to make it take effect'
 
@@ -12,11 +12,16 @@ test_and_move() {
     src=$1
     dst=$2
     printf '%s' "move $src to $dst..."
-    if [ -e $dst ]; then
-        printf '\n%s\n' "file exists: $dst"
+    if [ ! -e $src ]; then
+        printf '\n%s\n' "[ERROR] file not exists: $src"
         exit -1
     else
-        mv $src $dst
+        if [ -e $dst ]; then
+            printf '\n%s\n' "[ERROR] file exists: $dst"
+            exit -1
+        else
+            mv $src $dst
+        fi
     fi
     printf '  %s\n' 'done' 
 }
@@ -26,6 +31,7 @@ test_and_move '/tmp/qpwn/qpwn'     '/pwn/work/qpwn'
 test_and_move '/tmp/qpwn/qlibcdb'  '/pwn/work/qlibcdb'
 test_and_move '/tmp/qpwn/qmisc'    '/pwn/qmisc'
 test_and_move '/tmp/qpwn/test.py'  '/pwn/test.py'
+test_and_move '/tmp/qpwn/.vimrc'   '~/.vimrc'
 
 printf '%s' "delete /tmp/qpwn..."
 rm -r /tmp/qpwn
