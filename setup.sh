1acaff661b494520c79ae8f227bc86ff66dbeda0
QAQmander <salamander@buaa.edu.cn>
2019-07-10 00:56:56 +0800

    qpwn path bug fixed

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 git clone https://github.com/qaqmander/qpwn.git /tmp/qpwn
  
-printf '%s' "fix ~/.bashrc: add /pwn/bin to PATH..."
+printf '%s' "fix ~/.bashrc: add /pwn/qbin to PATH..."
 # TODO: maybe there is some way to change $PATH immediately
-echo 'export PATH=$PATH:/qpwn/bin' >> $HOME/.bashrc #&& source $HOME/.bashrc
+echo 'export PATH=$PATH:/pwn/qbin' >> $HOME/.bashrc #&& source $HOME/.bashrc
 printf '  %s\n' 'done' 
 echo '[NOTICE] You need to source ~/.bashrc to make it take effect'
 
