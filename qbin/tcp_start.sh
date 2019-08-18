cb5f1b7ea6e6ac5e25d7f8895c915ba51a5d9bfc
qaqmander <qaqmander@gmail.com>
2019-08-18 14:28:06 +0800

    change tcp_start args

--- qbin/tcp_start.sh
+++ qbin/tcp_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ $# < 1 ]]; then
-    echo 'Usage: ./tcp_start.sh PORT COMMAND'
+    echo 'Usage: ./tcp_start.sh COMMAND PORT'
     exit -1
 fi
 
-socat tcp-l:$1,fork exec:"$2",reuseaddr
+socat tcp-l:$2,fork exec:"$1",reuseaddr
